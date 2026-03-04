fa1e6983bf0d3d89cee1ea3374b41b427dcf6dff
sw76911 <sw76911@gmail.com>
2026-03-04 13:05:51 +0800

    Update data.js

--- data.js
+++ data.js
@@ -1,7 +1,36 @@
 // data.js
 const themes = {
     'Level 1：人物': [
-        { en: "Adult", cn: "成人", pos: "n.", sen: "A child should be with an adult.", trans: "小孩應該由成人陪同。" },
+        { en: "Adult", cn: "成人", pos: "n.", sen: "A child should be with an adult.", trans: "小孩應該由成人陪同。" },{ en: "Couple", cn: "情侶、夫妻", pos: "n.", sen: "They are a very happy ____.", trans: "他們是一對非常幸福的夫妻。" },
+{ en: "Customer", cn: "顧客", pos: "n.", sen: "The ____ is buying food in the shop.", trans: "顧客正在商店裡買食物。" },
+{ en: "Fool", cn: "呆子", pos: "n.", sen: "Don't be a ____; think before you act.", trans: "別當呆子，三思而後行。" },
+{ en: "Genius", cn: "天才", pos: "n.", sen: "Einstein was a great ____.", trans: "愛因斯坦是一位偉大的天才。" },
+{ en: "Gentleman", cn: "紳士", pos: "n.", sen: "He is a kind and polite ____.", trans: "他是一位善良且有禮貌的紳士。" },
+{ en: "Giant", cn: "巨人", pos: "n.", sen: "The ____ lives in a big castle.", trans: "巨人住在一座大城堡裡。" },
+{ en: "Girl", cn: "女孩", pos: "n.", sen: "The ____ is wearing a red dress.", trans: "那個女孩穿著一件紅色的洋裝。" },
+{ en: "Guest", cn: "客人、來賓", pos: "n.", sen: "We have a ____ coming for dinner.", trans: "我們有一位客人要來吃晚餐。" },
+{ en: "Guy", cn: "傢伙", pos: "n.", sen: "He is a nice ____.", trans: "他是個不錯的傢伙。" },
+{ en: "Hero", cn: "英雄", pos: "n.", sen: "Firemen are ____es in my heart.", trans: "消防員是我心目中的英雄。" },
+{ en: "Host", cn: "男主(持)人", pos: "n.", sen: "The ____ welcomed everyone to the party.", trans: "男主人歡迎大家參加聚會。" },
+{ en: "Kid", cn: "孩童、青年", pos: "n.", sen: "Don't be silly, ____.", trans: "孩子，別傻了。" },
+{ en: "King", cn: "國王", pos: "n.", sen: "The ____ wears a gold crown.", trans: "國王戴著金色的皇冠。" },
+{ en: "Lady", cn: "淑女、女士", pos: "n.", sen: "Ladies and gentlemen, welcome!", trans: "各位女士、先生，歡迎！" },
+{ en: "Male", cn: "男性", pos: "n./adj.", sen: "Is the cat a ____ or a female?", trans: "這隻貓是公的還是母的？" },
+{ en: "Man", cn: "男人", pos: "n.", sen: "That ____ is a doctor.", trans: "那個男人是一位醫生。" },
+{ en: "Master", cn: "主人、大師", pos: "n.", sen: "He is a ____ of cooking.", trans: "他是一位烹飪大師。" },
+{ en: "Neighbor", cn: "鄰居", pos: "n.", sen: "My ____ is very friendly.", trans: "我的鄰居非常友善。" },
+{ en: "Partner", cn: "夥伴", pos: "n.", sen: "She is my best dancing ____.", trans: "她是我最好的舞伴。" },
+{ en: "People", cn: "人們", pos: "n.", sen: "There are many ____ in the park.", trans: "公園裡有很多人。" },
+{ en: "Person", cn: "人", pos: "n.", sen: "She is a very kind ____.", trans: "她是一個非常善良的人。" },
+{ en: "Prince", cn: "王子", pos: "n.", sen: "The ____ lives in a castle.", trans: "王子住在城堡裡。" },
+{ en: "Princess", cn: "公主", pos: "n.", sen: "The ____ is very beautiful.", trans: "公主非常漂亮。" },
+{ en: "Queen", cn: "皇后", pos: "n.", sen: "The ____ lives in England.", trans: "皇后住在英國。" },
+{ en: "Stranger", cn: "陌生人", pos: "n.", sen: "Don't talk to a ____.", trans: "不要跟陌生人說話。" },
+{ en: "Teenager", cn: "青少年", pos: "n.", sen: "My brother is a ____ of 15.", trans: "我弟弟是個 15 歲的青少年。" },
+{ en: "Visitor", cn: "訪客", pos: "n.", sen: "There are many ____s in the museum.", trans: "博物館裡有很多訪客。" },
+{ en: "Woman", cn: "女人", pos: "n.", sen: "The ____ is my mother.", trans: "那位女人是我的母親。" },
+{ en: "Youth", cn: "青年", pos: "n.", sen: "Enjoy your ____ while you can.", trans: "趁年輕時好好享受青春。" },
+
         // ... 其他單字
     ],
     'Level 2：外貌與個性': [
