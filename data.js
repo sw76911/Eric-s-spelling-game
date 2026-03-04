70d376918e74a55838cf6b7f414995b7fd1f5a9e
sw76911 <sw76911@gmail.com>
2026-03-04 10:21:44 +0800

    Create data.js

---
+++ data.js
@@ -0,0 +1,14 @@
+// data.js
+const themes = {
+    'Level 1：人物': [
+        { en: "Adult", cn: "成人", pos: "n.", sen: "A child should be with an adult.", trans: "小孩應該由成人陪同。" },
+        // ... 其他單字
+    ],
+    'Level 2：外貌與個性': [
+        // ... 其他單字
+    ],
+    // 依此類推到 Level 30
+    'Level 30：進階主題': [
+        // ... 其他單字
+    ]
+};
